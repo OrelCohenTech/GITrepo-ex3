1f801685cd007b0e052cab65d94fc22de4821ed3
Orel Cohen <ORELCOHEN953@GMAIL.COM>
2026-04-20 12:09:26 +0300

    Update orel.java

--- orel.java
+++ orel.java
@@ -1,7 +1,7 @@
 public class orel {
     public static void main(String[] args) {
         for(int i=0; i<5; i++) {
-            System.out.println("Orel Cohen Version 2");
+            System.out.println("Orel Cohen");
         }
     }
 }
